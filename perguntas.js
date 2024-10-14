cfca08eef1436f90f0b4b16e7b8fb74cdbaa18fc
2AKMC <167830225+2AKMC@users.noreply.github.com>
2024-10-14 14:09:38 -0300

    Up3

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'English',
+    'Como se diz oi em English?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Motivacional',
+    'Como ser mais inteligente?',
+    'Estudando mais'
+)
+criaCartao(
+    'Motivacional',
+    'Você é melhor que isso?',
+    'Com plena certeza posso dizer que sou'
+)
+criaCartao(
+    'Español',
+    'Como se diz olá em Español?',
+    'Hola'
+)
+criaCartao(
+    'Motivacional',
+    'Você realmente chegou ao seu limite?',
+    'Não, ainda estou nos 40%'
+)
+criaCartao(
+    'Matemática',
+    'Raíz quadrada de 100?',
+    '10'
+)
